31b1ba62bd61299e169fa5f2cc5cb1e785313f99
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-07-25 13:47:24 -0400

    Scrolling background loop

---
+++ Happy Bird Game/Assets/Scenes/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = target.transform.position.x + .005f;
+        position.y = target.transform.position.y;
+        transform.position = position;
+    }
+}
