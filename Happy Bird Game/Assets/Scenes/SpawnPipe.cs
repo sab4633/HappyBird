e82f98bb3acba8d9ad1c4e4ad75c4fd82cb23775
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-07-25 20:45:00 -0400

    Update SpawnPipe.cs

--- Happy Bird Game/Assets/Scenes/SpawnPipe.cs
+++ Happy Bird Game/Assets/Scenes/SpawnPipe.cs
@@ -38,15 +38,16 @@ public class SpawnPipe : MonoBehaviour
         //Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane+5)); //will get the middle of the screen
         
         Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Random.Range(bounds, Screen.height-bounds), Camera.main.farClipPlane / 2));
-        GameObject clone = Instantiate(pipe) as GameObject;
-        clone = (GameObject)Instantiate(pipe, screenPosition, Quaternion.identity);
-        pipes.Add(clone);
+       // GameObject clone = Instantiate(pipe) as GameObject;
+        Instantiate(pipe, screenPosition, Quaternion.identity);
+        /*pipes.Add(clone);
         if (pipes.Count >= 3)
         {
             Destroy(pipes[0]);
             pipes.RemoveAt(0);
             Debug.Log(pipes.Count);
         }
+        */
     }
     /*
     public void DestoryPipes()
